e3225737bee7ef4821f01a0938a6a4338ca582e9
mohamedkhaled <mohamedkhaled281@outlook.com>
2025-09-18 18:07:19 +0300

    Initial commit - Spring project

---
+++ src/main/java/com/gr74/task_managment_system/TaskManagmentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.gr74.task_managment_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagmentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagmentSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gr74/task_managment_system/TaskManagmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gr74.task_managment_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagmentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
